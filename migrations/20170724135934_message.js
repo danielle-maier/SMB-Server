364e6407325a1421fb988108eb64a1110e196921
Danielle Maier <damaier@gmail.com>
2017-07-24 20:20:56 -0600

    😅 db fixes

--- migrations/20170724135934_message.js
+++ migrations/20170724135934_message.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("message", (message) => {
     message.increments("id");
     message.varchar("name").notNullable();
-    message.varchar("title").notNullable();
+    message.varchar("title").notNullable().unique();
     message.text("messageBody").notNullable();
     message.dateTime('createdAt').notNullable().defaultTo(knex.raw('now()'));
     message.dateTime('updatedAt').notNullable().defaultTo(knex.raw('now()'));
--- seeds/01_messages.js
+++ seeds/01_messages.js
@@ -7,36 +7,26 @@ exports.seed = function(knex, Promise) {
       name: 'Dani',
       title: 'well well well',
       messageBody: 'Let me out.'
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     }, {
       id: 2,
       name: 'Dani',
       title: 'well...',
       messageBody: 'This is not a dance'
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     },{
       id: 3,
       name: 'Angela',
       title: 'wellichy',
       messageBody: 'I am the one who\'s been here for far too long'
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     },{
       id: 4,
       name: 'Angela',
       title: 'you can\'t be what you were',
       messageBody: 'This is not a dance.'
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     },{
       id: 5,
       name: 'Jamie',
-      title: 'well well well',
+      title: 'well well',
       messageBody: 'In the midnight hour, she cried more more more'
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     }];
 
     return knex('message').insert(messages);
