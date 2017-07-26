038265b5e105c656d86c7282611bc81a5230de0b
Danielle Maier <damaier@gmail.com>
2017-07-26 10:59:24 -0600

    get by id 👽 works

--- queries.js
+++ queries.js
@@ -3,5 +3,9 @@ const knex = require("./db/knex");
 module.exports = {
   getMessages: function() {
     return knex('message');
+  },
+  getMessageByID: function(message_id){
+    return knex('message')
+    .where('id', message_id);
   }
 };
--- routes/messages.js
+++ routes/messages.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const queries = require("../queries.js");
 
 router.get("/", (req, res) => {
-    queries.getMessages()
-    .then(function(messages){
-        res.json(messages);
+  queries.getMessages()
+    .then(function(messages) {
+      res.json(messages);
     });
 });
 
-router.get("/:id", (req,res) => {
-  queries.getMessageByID(req.prams.id)
-  .then(function(message){
-    res.json(message);
-  });
-)};
+router.get("/:id", (req, res) => {
+  queries.getMessageByID(req.params.id)
+    .then(function(message) {
+      res.json(message);
+    });
+});
 
 
 module.exports = router;
